9c927d4db0dbf7fd39f5c5169fed24d9f65a968f
Dylan Greene <dylang@gmail.com>
2014-10-17 18:08:19 -0400

    * npm-check works even when node modules are not installed * ignore devDeps * skip unused check * start of interactive update

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,3 @@
 #!/usr/bin/env node
 
-var cwd = process.cwd();
-var pkgJson = require('../package.json');
-var npmCheck = require('../lib/npm-check');
-var output = require('../lib/output');
-
-process.title = pkgJson.name;
-
-npmCheck(cwd)
-    .then(output)
-    .catch(function(err){
-        console.log('[npm-check]', err.stack || err);
-        process.exit(1);
-    })
-    .done()
+require('../lib/cli');
