b18ccfa21d7c9760e69a74a46eee49ad1cb7e1b2
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2017-05-11 14:32:51 -0700

    Clean up README & finish devOnly feature (#220)
    * Remove Grunt README compilation
    
    * Finish devOnly feature
    
    Implement incompete devOnly feature that was documented in the README.
    
    * Update README
    
    - Remove messaging about / formatting from README autogeneration
    - Update API section order consistency with CLI section order
    - Update CLI / add API documentation language for "debug" option
    - Add missing API documentation for "ignore" option
    
    * Update README with correct "ignore" default value

--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// This is going away soon.
-// It is still used for the readme.
-// $ grunt repos && grunt readme
-module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
-};
--- templates/npm-check.md
+++
@@ -1,8 +0,0 @@
-
-{%= _.doc('readme/features.md') %}
-
-{%= _.doc('readme/cli.md') %}
-
-{%= _.doc('readme/api.md') %}
-
-{%= _.doc('readme/inspiration.md') %}
