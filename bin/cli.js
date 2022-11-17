075b6448c95f1d09533e1c4368e23aa7686d215a
rouanw <rouanw@users.noreply.github.com>
2022-11-17 10:10:36 +0200

    chore!: drop es5 support
    BREAKING CHANGE: no longer offer an es5 version of the module

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 
-var isEs2015;
-try {
-    isEs2015 = new Function('() => {}');
-} catch (e) {
-    isEs2015 = false;
-}
-isEs2015 ? require('../lib/cli') : require('../lib-es5/cli');
+require('../lib/cli');
